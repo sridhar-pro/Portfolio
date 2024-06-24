a47dc53e75707da54a8687896d057ba941795c86
Sridhar <sriisridhar81@gmail.com>
2024-06-24 12:20:25 +0530

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:aakash10aj@gmail.com">sriisridhar81@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/sridhar-s-28412224b">linkedin.com/Sridhar</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/sridhar-pro/">github.com/Sridhar</a>
+        </li>
+      </ul>
+      
+    </footer>
+  );
+};
